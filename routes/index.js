0b6c5e526ab7a9d44fd70b58172774313220de81
mtyiu <mtyiu@cse.cuhk.edu.hk>
2016-03-22 17:18:05 +0800

    Finish Lab 4

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,13 @@ var express = require('express');
 var path = require('path');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
+router.get( '/', function( req, res ) {
+	var id = Math.floor( Math.random() * 10000 );
+	res.redirect( '/' + id );
+} );
+
+router.get( '/:id([0-9]+)', function( req, res ) {
 	res.sendFile( path.resolve( __dirname + '/../views/index.html' ) );
-});
+} );
 
 module.exports = router;
